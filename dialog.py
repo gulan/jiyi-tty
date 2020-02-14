a171ad113265715bdc810d9aa550d6f0f5ae2eba
gulan <glen.wilder@gmail.com>
2020-02-13 17:00:17 -0800

    name dialog states with constants instead of literal strings

--- dialog.py
+++ dialog.py
@@ -5,64 +5,55 @@ import time
 
 VERSION = '4.0.0'
 
+FLIPA     = '0-user-prod-a'
+FLIPB     = '0-user-prod-b'
+SCORE     = '0-user-score'
+ITER      = '1-iter-again'
+ITER_END0 = '0-iter-end'
+ITER_END1 = '1-iter-end'
+EXIT      = 'EXIT'
+
 def loop(gs,sc,log):
     log.write("---- new play ----\n")
     start = time.time()
     log.write("T> %s\n" % time.strftime("%Y-%m-%d %H:%M"))
     tossed = 0
-    state = '1-iter-again'
-    while state != 'EXIT':
+    state = ITER
+    while state != EXIT:
         
-        if state == '1-iter-end':
+        if state == ITER_END1:
             gs.restack()
-            state =  '0-iter-end' if gs.gameover else '1-iter-again'
+            state =  ITER_END0 if gs.gameover else ITER
             continue
             
-        if state == '1-iter-again':
+        if state == ITER:
             sc.display_question(gs.question)
-            state = '0-user-prod-a'
+            state = FLIPA
             continue
         
-        if state == '0-user-prod-a':
+        if state == FLIPA:
             sc.accept_flip()
-            state = '0-user-prod-b'
+            state = FLIPB
             continue
         
-        if state == '0-user-prod-b':
+        if state == FLIPB:
             sc.display_answer(gs.answer,tossed)
-            state = '0-user-score'
+            state = SCORE
             continue
         
-        if state == '0-user-score':
+        if state == SCORE:
             if sc.accept_score() == screen.TOSS:
                 gs.toss()
                 tossed += 1
             else:
                 gs.keep()
-            state = '1-iter-again' if gs.more else '1-iter-end'
+            state = ITER if gs.more else ITER_END1
             continue
             
-        if state == '0-iter-end':
+        if state == ITER_END0:
             gs.check_endgame()
             duration = int(time.time() - start)
             log.write("D> %s\n" % duration)
-            state = 'EXIT'
+            state = EXIT
             continue
 
-# def dialog():
-#     "Run game with a screen server process."
-#     gs = chinese.SQL(10)
-#     try:
-#         sc = screen.screen_xmlrpc()
-#         loop(gs,sc)
-#     finally:
-#         sc.cleanup()
-
-# def dialog_local(count=20):
-#     "Run game as a regular program, no IPC. See play.py"
-#     log = open('dialog.log', 'a')
-#     gs = chinese.SQL(count)
-#     sc = screen.new_screen(log)
-#     loop(gs,sc,log)
-#     sc.cleanup()
-#     log.close()
