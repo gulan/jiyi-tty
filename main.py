a52e61a2b6051adb7cdf0ebef64eea629a7335a4
gulan <glen.wilder@gmail.com>
2020-02-15 00:22:10 -0800

    remove xmlrpc stuff

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from screen import new_screen
+from screen import screen
 from chinese import SQL
 from dialog import loop
 
 def main(count=12):
     db = SQL(count)
-    device = new_screen()
+    device = screen()
     loop(db,device)
     device.cleanup()
 
--- screen.py
+++ screen.py
@@ -1,42 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*- 
 
-# Class screen was orignally made to be imported into a standalone
-# game. The xmlrpc wrapper was added later.
-
-# It might seen strange that talking to a screen means going though a
-# server, but this setup is common. We own one centralized database,
-# but there could be many remote players.
-
-# As an xml server, the module must be running before the user tries
-# running the game (dialog.py).
-
-# ansi_flashcard does not yet have a simple launcher that will start
-# the screen server only if needed, before starting the dialog
-# process. This might be convenient now, but is of no use for a
-# distributed system, where the server must always be runnung.
-
-# xmlrpc allows me to convert an object into a proxy to a server. The
-# original object is in the server. The proxy implelemts the objects
-# methods, but turns them into message that are sent to the
-# server. the goal is to transparently replace the object in an
-# application with the proxy.
-
-# of course, the create method for the object and server will differ,
-# but once created, they act the same. what about destruction? does
-# the end of the game also destroy the server? do the server need to
-# add to the interface of screen?
-
 import curses
 import os
-import sys
-import xmlrpc.client
-from xmlrpc.server import SimpleXMLRPCServer
-from xmlrpc.server import SimpleXMLRPCRequestHandler
 
-TOSS,KEEP = 'TOSS','KEEP'
-ENTER = 10
-DELETE = curses.KEY_DC
+TOSS, KEEP    = 'TOSS', 'KEEP'
+ENTER, DELETE = 10, curses.KEY_DC
 
 class screen:
 
@@ -84,42 +53,3 @@ class screen:
         self.stdscr.keypad(0)
         curses.echo()
         curses.endwin()
-
-def run_screen_server(addr):
-
-    class RH(SimpleXMLRPCRequestHandler):
-        rpc_paths = ('/RPC2',)
-
-    server = SimpleXMLRPCServer(addr, requestHandler=RH, logRequests=False, allow_none=True)
-    server.register_instance(screen())
-    server.serve_forever()
-    sys.exit()
-
-class app:
-    def __init__(self, server, proxy):
-        self.server = server
-        self.proxy = proxy
-
-    def __getattribute(self, name):
-        try:
-            return object.__getattribute__(self, name)
-        except AttributeError:
-            proxy = object.__getattribute__(self, 'proxy')
-            return getattr(proxy, name)
-
-    def terminate(self):
-        server = object.__getattribute__(self, 'server')
-        server.terminate()
-        server.join()
-
-def new_screen(local=True):
-    # Client gets an instance that behaves like a screen, but cannot
-    # tell if it is local or remote.
-    if local:
-        return screen()
-    else:
-        addr = ("localhost", 8005)
-        s = multiprocessing.Process(target=run_screen_server, args=(addr,))
-        s.start()
-        c = xmlrpc.client.ServerProxy('http://%s:%s' % addr)
-        return app(s, c)
