bd7f4d9f6dc13668906a36aa0ae1e8b4ede498cd
gulan <glen.wilder@gmail.com>
2020-02-14 17:36:39 -0800

    Delete working, but uninteresting, list implementation of the database

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 
-# Moved main() from chinese.py, as that module should not know about
-# the dialog and screen modules.
-
-# Should the database have a cleanup()?
-
-import screen
-import chinese
-import dialog
+from screen import new_screen
+from chinese import SQL
+from dialog import loop
 
 def main(count=12):
-    gs = chinese.SQL(count)
-    sc = screen.new_screen()
-    dialog.loop(gs,sc)
-    sc.cleanup()
+    db = SQL(count)
+    device = new_screen()
+    loop(db,device)
+    device.cleanup()
 
 main()
 
